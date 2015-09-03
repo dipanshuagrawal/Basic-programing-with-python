1efcace4bd8c9849900c1c7b646a2c000642bd77
Dipanshu <deep14nit@gmail.com>
2015-09-03 20:16:20 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,40 @@
+#Here we will be discussing about different type of operator that we can use in Python
+
+#Arithmetics and Comparision Operators
+a = 5+6;
+b = 6-5;
+c = 5*6;
+d = 6/5;
+e = 6%5;
+f = 3**2;
+g = (a==b);
+h = (a>b);
+i = (a>=b);
+j = (a<b);
+k = (a<=b);
+
+#Bitwise and Logical Operators
+a=34;
+b=56;
+#Give binary value of a.
+bin(a);  
+
+#Give binary value of b
+bin(b);  
+
+#AND operator
+bin(a&b);  
+
+#OR operator
+bin(a|b);  
+
+#Exclusive OR operator
+bin(a^b);  
+
+
+#Logical Operators
+a=True;
+b=False;
+a and b;
+a or b;
+not a;
