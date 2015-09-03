1f00ba1c3376e1b89064837003411b9c5a431c42
Dipanshu <deep14nit@gmail.com>
2015-09-03 19:48:28 +0530

    Create Data_type_conversion.py

---
+++ Data_type_conversion.py
@@ -0,0 +1,35 @@
+#To convert one data type to another we have some built in function in Python.
+
+#To convert decimal value to integer.
+int(55.3)
+
+#To convert integer value to decimal.
+float(44)
+
+#To convert integer value to string.
+strVar = str(3456)
+strVar[3]
+
+#To convert string value to tuple.
+tuple("We are dealing with python programming")
+
+#To convert string value to list.
+list["We are dealing with python programming"]
+
+#To convert ASCII value to character
+chr(65)
+
+#To convert character to ASCII
+ord('A')
+
+#To convert integer to Hex
+hex(65)
+
+#To convert integer to oct
+oct(65)
+
+
+
+
+
+
